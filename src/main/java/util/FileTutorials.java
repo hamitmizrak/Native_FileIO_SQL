37bdeb916ffb23cb0d81545b2803b5037f3c388a
hamitmizrak <hamitmizrak@gmail.com>
2023-06-07 11:25:34 +0300

    File

---
+++ src/main/java/util/FileTutorials.java
@@ -0,0 +1,45 @@
+package util;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTutorials {
+    public static void main(String[] args) throws IOException {
+        try {
+            // C:\io\ecodation\eco9.txt
+            File file = new File(MyPathName.MY_PATH_NAME);
+
+            System.out.println("### IS ###");
+            System.out.println(file.isFile());
+            System.out.println(file.isDirectory());
+            System.out.println("SAKLI MI "+file.isHidden());
+
+            System.out.println("### CAN ###");
+            System.out.println("Read: " + file.canRead());
+            System.out.println("Write: " + file.canWrite());
+            System.out.println("Execute: " + file.canExecute());
+
+            System.out.println("###  ###");
+            System.out.println("setExecutable: " + file.setExecutable(false));
+            System.out.println("setWritable: " + file.setWritable(false));
+            System.out.println("setReadable: " + file.setReadable(false));
+
+            System.out.println("Read: " + file.canRead());
+            System.out.println("Write: " + file.canWrite());
+            System.out.println("Execute: " + file.canExecute());
+
+            System.out.println("### GET ###");
+            System.out.println("ADI: " + file.getName());
+            System.out.println("PATH: " + file.getPath());
+            System.out.println("PARENT FILE: " + file.getParentFile());
+            System.out.println("FREE SPACE: " + file.getFreeSpace());
+            System.out.println("TOTAL SPACE: " + file.getTotalSpace());
+            System.out.println("Absolute Path: " + file.getAbsolutePath());
+            System.out.println("Canonical Path: " + file.getCanonicalPath());
+
+            // PATH, Absolute Path, Canonical Path
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/util/MyPathName.java
@@ -0,0 +1,5 @@
+package util;
+
+public class MyPathName {
+    public static final String MY_PATH_NAME="C:\\io\\ecodation\\eco9.txt";
+}
